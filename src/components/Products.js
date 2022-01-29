536c1c0e4f80bc42c6877f615630ac8894babe82
Michelle M <michellemalfabon@gmail.com>
2022-01-28 17:04:57 -0800

    products: not complete, need help

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+// import getSomething from "../api" 
 
 //axios is throwing me off, not sure how to fetch from backend- haaalp :/
-
 
 const Products= ({currentUser, token}) =>{
 const [product, setProduct]= useState({});
@@ -21,13 +20,22 @@ const [product, setProduct]= useState({});
      }
  }, []);
 
-//  return (
-// <>
+ //OR const fetchAllProducts = async (token) => {
+//     const products = await getSomething({
+//         url: '/products',
+//         method: 'GET',
+//         token,
+//     });
+//     return products;
+// };
 
+return (
+<>
+<h1> Our Products</h1>
 
+</>
 
-
-//  )
+)
 
 
 
