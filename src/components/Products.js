ec90a9f9b608943e24339043055b47502eb13648
Kriselda Bonifacio <88959891+kbonifacio@users.noreply.github.com>
2022-01-30 21:33:02 -0800

    Kriselda/products page (#47)
    * added product api routes and router
    
    * fixing merge conflicts to pull current dev branch
    
    * code review 2 fixes #2
    
    * feat(productPage): Displayed products and styled each card
    
    Co-authored-by: Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./Products.css"
 
 const Products = () => {
     const [products, setProducts] = useState([]);
@@ -19,7 +20,42 @@ const Products = () => {
     useEffect(fetchProducts, []); 
 
     return <>
-    <h1>Products</h1>
+    <h1 className="productHeader">Our Wines and Cheeses</h1>
+    <div className="productCardContainer">
+        <div className="productCard">
+            {
+                products.map((product)=> {
+                    return (
+                        <>
+                            <div key={product.id}>
+                                <div className="cardContentContainer">
+                                    <div className="cardContent">
+                                        {product.name}
+                                        <img src={product.imgURL} className="productImg"></img>
+                                    </div>
+                                    <div className="itemPrice">
+                                        ${product.price}
+                                    </div>
+                                    <div className="productButtonsContainer">
+                                        <button className="vProdButton" >View Product</button>
+                                        <button className="addToCartButton">Add to Cart</button>
+                                    </div>
+                        
+                                </div>
+                                
+                                
+                                
+                                {/* name, img, price, buttons x2 */}
+                                {/* button1: add to cart (possibly with +/- or an input field for how many desired) */}
+                                {/* button2: to single card view */}
+                            </div>
+                        </>
+                    )
+                })
+            }
+        </div>
+    </div>
+    
     </>
 }
 
