3d537dbea122d808abf29927d4cd4aa2db656262
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-02-02 19:18:11 -0800

    Update db.spec.js

--- tests/db.spec.js
+++ tests/db.spec.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const bcrypt = require('bcrypt');
 const SALT_COUNT = 10;
-
+const { rebuildDB } = require('../db/seedData');
 const { createUser, 
     getUser, 
     getUserById, 
