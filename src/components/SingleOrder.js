6d249fa592946ab736fbbac14b6a59468f0aa6a0
Richard Brown <69214042+number1ricky@users.noreply.github.com>
2022-02-10 19:01:47 -0800

    final one for 2/10

---
+++ src/components/SingleOrder.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./SingleOrder.css"
+
+const SingleOrder =({orderId, getOrdersByUser, userID, token}) =>{
+const [orderProducts, setOrderProducts] = useState({});
+
+    retrun (
+        <>
+        <h1>Review Products Ordered:</h1>
+        
+        </>
+    )
+}
+
+
+export default SingleOrder
