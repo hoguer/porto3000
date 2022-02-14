d3d05d48b8acd1fea5d1ef7f3c3327309e1ac2a0
Michelle M <michellemalfabon@gmail.com>
2022-02-13 23:18:06 -0800

    styled home

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,19 +18,25 @@ const Home = () => {
         <div className="featuredContainer">
             <img src={img2} className="img2"/>
             <img src={img1} className="img1"/>
-            {/* <img src={img2} className="img2"/> */}
+            <div><h2><i>Online Reservations Available Soon</i></h2>
+            <p>OPEN 10:00 AM- 7:00 PM daily</p>
+            <br></br>
+            <p>3000 Local Host Dr. </p>
+            <p>Sonoma, CA</p>
+            <p>(707) 767-8300</p>
+            </div>
             
         </div>
         <div className="featuredContainerRight">
             <div className="featured">
                 <div className="featContainerParagraph">
-                    <h1>Hello!</h1>
+                    {/* <h1>Hello!</h1> */}
                     <div>
                         <p>Welcome to Porto 3000. Our goal is to deliver the finest wines and cheeses to your familia's table. We believe that wine and cheese are a wonderful pairing, but we also believe that it can foster great experiences between new aquaintances and loved ones. The ingredients used to make our products are ethically sourced and USDA organic certified. Additionally, an ideal that we hold near and dear to our hearts is that all of our ingredients must be sourced locally. We love to support our friends and neighbors along their business journies so that we can grow and succeed as a community. May our wine bring you and your family wonderful new memories and we hope to see you soon on the vineyard. </p>
                     </div>
                 </div>
                 <div className="featHeader">
-                    <h1>Featured Products</h1>
+                    <h1><i>Featured Products</i></h1>
                 </div>
                 <div className="featProdCards">
                     <div className="featProdL">
@@ -43,7 +49,7 @@ const Home = () => {
                             {/* <p>Goat Cheese</p> */}
                         </div>
                         <div className="featProdR">
-                            <NavLink to="/products/40"><img src={featImg3} className="featImgR"/></NavLink> 
+                            <NavLink to="/products/36"><img src={featImg3} className="featImgR"/></NavLink> 
                             {/* <p>Oscypki</p> */}
                         </div>
                     </div>
