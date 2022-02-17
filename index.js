da5121f993bf79f6c61e1cdd3ec2c0a32cc61203
Rachel Smith <rachel.smith@fullstackacademy.com>
2022-02-16 20:36:18 -0700

    clean up package.json

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ server.use(express.json());
 const path = require('path');
 server.use(express.static(path.join(__dirname, 'build')));
 
-server.disable('etag')
-
 server.use(async (req, res, next) => {
   try {
     const auth = req.header('Authorization'); 
@@ -29,10 +27,7 @@ server.use(async (req, res, next) => {
       token = token.trim();
 
       const userObj = jwt.verify(token, JWT_SECRET);
-      console.log("userObj", userObj)
-
       req.user = await getUserById(userObj.id);
-      console.log("req.user", req.user)
 
       next();
     }
